a4e9e50523f4b056ec4b8d72fbcd947097072bc0
Vlad <vladbashak80@gmail.com>
2024-11-20 00:17:13 +0100

    fix upload base

--- components/consts.ts
+++ components/consts.ts
@@ -1,7 +1,7 @@
 export const LOCAL_STORAGE_KEY = 'tasksOrder'
 export const LOCAL_STORAGE_CATEGORY_ORDER_KEY = 'categoriesOrder'
 export const BASE_URL = process.env.DB_URL
-	? process.env.DB_URL?.replace('/api', '')
+	? process.env.DB_URL?.replace('/api/', '')
 	: 'http://localhost:5000'
 
 export const months = [
