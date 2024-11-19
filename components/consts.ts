b23616c3788070024a368957cec18af4adb10a72
Vlad <vladbashak80@gmail.com>
2024-11-20 00:09:10 +0100

    image fix

--- components/consts.ts
+++ components/consts.ts
@@ -1,6 +1,9 @@
 export const LOCAL_STORAGE_KEY = 'tasksOrder'
 export const LOCAL_STORAGE_CATEGORY_ORDER_KEY = 'categoriesOrder'
-export const BASE_URL = process.env.DB_URL?.replace('/api', '')
+export const BASE_URL = process.env.DB_URL
+	? process.env.DB_URL?.replace('/api', '')
+	: 'http://localhost:5000'
+
 export const months = [
 	'January',
 	'February',
